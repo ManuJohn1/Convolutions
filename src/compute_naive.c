e4b5fcf6ac4d28e6c203526569897ee218d7fedd
Manu John <manujohn.25@berkeley.edu>
2023-05-02 16:46:57 -0700

    finished task 1

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -14,36 +14,35 @@ int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
   // output_matrix
-  matrix_t *res_matrix = malloc(sizeof(struct matrix_t));
+  matrix_t *res_matrix = malloc(sizeof(matrix_t));
   res_matrix->rows = a_matrix->rows - b_matrix->rows + 1;
   res_matrix->cols = a_matrix->cols - b_matrix->cols + 1;
-  res_matrix->data = malloc(sizeof(res_matrix->rows*res_matrix->cols));
+  res_matrix->data = malloc(sizeof(int32_t) * res_matrix->rows*res_matrix->cols);
 
 
   int32_t b_len = b_matrix->rows*b_matrix->cols;
   int32_t *vec_b_flipped = malloc(sizeof(int32_t)*b_len);
-  int32_t *vec_a = malloc(sizeof(int32_t)*b_len);
 
   for (int32_t i=0; i< b_len; i++) {
-	  vec_b_flipped[i] = b_matrix->data[b_len-i];
+	  vec_b_flipped[i] = b_matrix->data[b_len-i-1];
   }
 
   
 
   for (int32_t p=0; p < res_matrix->rows; p++) {
 	for (int32_t q=0; q < res_matrix->cols; q++) {
-		int32_t index = 0; 
-		for(int32_t i=0; i < b_matrix->rows; i++) {
-			for(int32_t j=0; j < b_matrix->cols; j++) {
-				vec_a[index] = a_matrix->data[(p*i)*a_matrix->cols + (q+j)];
-				index = index + 1;		
-			} 
-		}
-		dot_product = dot(b_len, vec_a, vec_b_flipped);
-		res_matrix->data[p*res_matrix->cols + q] = dot_product;
+		
+        int32_t sum = 0;
+        for(int32_t i=0; i < b_matrix->rows; i++) {
+            sum += dot(b_matrix->cols, (a_matrix->data + (p+i)*a_matrix->cols + q), (vec_b_flipped + (i)*b_matrix->cols));
+        }
+
+		res_matrix->data[p*res_matrix->cols + q] = sum;
   	} 
-  } 
-  output_matrix = &res_matrix;
+  }
+  //free(vec_b_flipped);
+  //free(vec_a);
+  *output_matrix = res_matrix;
   return 0; 
 
 }
