6d783e448f481ec1ee29d0063c2dcf725f248b8a
Manu John <manujohn.25@berkeley.edu>
2023-05-03 12:31:28 -0700

    working on task 2

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -6,7 +6,30 @@
 // Computes the dot product of vec1 and vec2, both of size n
 int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   // TODO: implement dot product of vec1 and vec2, both of size n
+    dot = 0;
+    for(uint32_t i = 0; i < n/16 * 16; i += 16) {
+      __m256i vec_1a = _mm_loadu_si256((__m256i *) (vec1 + i));
+      __m256i vec_2a = _mm_loadu_si256((__m256i *) (vec2 + i));
+      __m256i sum_1 = _mm256_mullo_epi32(vec_1a, vec_2a);
 
+
+      int32_t tmp_arr[8];
+      _mm_storeu_si256((__m256i *) tmp_arr, sum_1);
+      dot += tmp_arr[0] + tmp_arr[1] + tmp_arr[2] + tmp_arr[3] + tmp_arr[4] + tmp_arr[5] + tmp_arr[6] + tmp_arr[7];
+
+      __m256i vec_1b = _mm_loadu_si256((__m256i *) (vec1 + i + 8));
+      __m256i vec_2b = _mm_loadu_si256((__m256i *) (vec2 + i + 8));
+      __m256i sum_2 = _mm256_mullo_epi32(vec_1b, vec_2b);
+
+      int32_t tmp_arr2[8];
+      _mm_storeu_si256((__m256i *) tmp_arr2, sum_2);
+      dot += tmp_arr2[0] + tmp_arr2[1] + tmp_arr2[2] + tmp_arr2[3] + tmp_arr2[4] + tmp_arr2[5] + tmp_arr2[6] + tmp_arr2[7];
+
+  }
+
+    for(unsigned int i = n/16 * 16; i < n; i++) {
+        dot += vec1[i]*vec2[i];
+    }
   return -1;
 }
 
